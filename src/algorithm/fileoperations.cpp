1c8b6a03c3c9553761e80ddbd7b6315d0434cd33
aalekh <aalekh.nigam@gmail.com>
2016-07-21 17:48:45 +0530

    Structured Fileoperations.*

--- src/algorithm/fileoperations.cpp
+++ src/algorithm/fileoperations.cpp
@@ -1,61 +1,115 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <utility>
+#include <map>
+#include <cstring>
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/parallel_algorithm.hpp>
+
+#include <boost/range/functions.hpp>
+
 #include "fileoperations.h"
 
 using namespace std;
 
-namespace hpx
-{
-	namespace flow{
-    	class hpxflow{
-        	public:
-            
-            string buffer, inter_buffer;
-            char c;
 
-            hpxflow(string filename) { //This constructor simply reades file input
-                ifstream inf(filename);
-                
-                // Although and expensive operation, I appened buffer char by char to have new line or tab space character included.
-                while (inf.get(c))
-                    buffer += c;
+hpxflow::hpxflow(string filename) { //This constructor simply reades file input
+    ifstream inf(filename);
+
+// Although and expensive operation, I appened buffer char by char to have new line or tab space character included.
+    while (inf.get(c))
+        buffer += c;
+
+    toVector();
+}
+
+void hpxflow::toVector(){
+    istringstream iss(buffer);
+    string word;
+    while(iss >> word) {
+        veco.push_back(word);
+    }
+    // for(int i = 0; i < veco.size(); i++){
+    //     cout << "Dump: " << veco[i] << endl;
+    // }
+    // return *this;
+}
+
+hpxflow &hpxflow::character_operation(char character, char replace_with){
+    using hpx::util::unwrapped;
+    using hpx::lcos::local::dataflow;
+    using hpx::parallel::for_each;
+    using hpx::parallel::par;
+    string app;
+    std::vector<char> data(buffer.begin(), buffer.end());
+    for_each(par, data.begin(), data.end(),
+        [&app, &character, &replace_with](char b)
+        {
+            if(to_string(b).c_str() == to_string(character).c_str()){
+
+                app = app + replace_with;
+
+            } else{
+
+                app = app + b;
+
             }
+    });
+    // }
+    buffer = app;
+    return *this;
+}
 
-            hpxflow &characterOperation(char character, char replace_with){
-                string app;
-                for(int i = 0; i < strlen(buffer.c_str()); i++){
-                    if(buffer[i] == character){
+hpxflow &hpxflow::write_to_file(string filename){
+    std::ofstream out(filename);
+    out << buffer;
+    out.close();
+    return *this;
+}
 
-                        app = app + replace_with;
+hpxflow &hpxflow::dump() {
+    for (map<string, string>::iterator it=inter_map.begin(); it!=inter_map.end(); ++it)
+        cout << "First :"<< it->first << " Second: " << it->second << '\n' << endl;
+    return *this;
+}
 
-                    } else{
-
-                        app = app + buffer[i];
-
-                    }
-                }
-                // }
-                buffer = app;
-                return *this;
+template <typename F, typename... Args>
+hpxflow &hpxflow::filter(int index, F fn, Args... args){
+    for(size_t i = 0; i < buffer_pair_intermediate.size(); i++) {
+        if(index == 1){
+            if(!fn(buffer_pair_intermediate[i].first, args...)){
+                buffer_pair_intermediate.erase(buffer_pair_intermediate.begin() + i);
             }
-
-            hpxflow &write_to_file(string filename){
-                std::ofstream out(filename);
-                out << buffer;
-                out.close();
-                return *this;
+        }else if(index == 2){
+            if(!fn(buffer_pair_intermediate[i].second, args...)){
+                buffer_pair_intermediate.erase(buffer_pair_intermediate.begin() + i);
             }
-
-
-        //     for (auto const &value : values) {
-        //         f(value);
-        //     }
-        // } 
-
-
-        };
-
+        }
     }
 }
-//int main(){
-//    hpxflow("text.txt").character_operation('\t', '\n').write_to_file("output.txt");
-//    return 0;
-//}
+
+template <typename L>
+hpxflow &hpxflow::reduce(L fn, int vl){
+    fn(vl, buffer_pair_intermediate, inter_map);
+    return *this;  
+}
+
+template <typename T>
+hpxflow &hpxflow::mapper(T t){
+    for (auto const &value : veco) {
+        buffer_pair.push_back(make_pair(t(value).first, t(value).second));
+        // auto v = t(value);
+        // cout << "First: " << v.first << " Second: " << v.second << endl;
+    }
+    buffer_pair_intermediate = buffer_pair;
+    return *this;
+    // return std::make_pair (10,20)
+}
+
+
+
--- src/algorithm/fileoperations.h
+++ src/algorithm/fileoperations.h
@@ -1,8 +1,6 @@
 #ifndef FILEOPERATIONS_H
 #define FILEOPERATIONS_H
 
-#include <fstream>
-#include <iostream>
 #include <string>
 #include <sstream>
 #include <cstdlib>
@@ -10,11 +8,6 @@
 #include <utility>
 #include <map>
 #include <cstring>
-#include <hpx/hpx.hpp>
-#include <hpx/hpx_init.hpp>
-#include <hpx/include/parallel_algorithm.hpp>
-
-#include <boost/range/functions.hpp>
 
 using namespace std;
 
@@ -34,98 +27,24 @@ class hpxflow{
 
         map<string, string> inter_map;
 
-        hpxflow(string filename) { //This constructor simply reades file input
-            ifstream inf(filename);
-        
-        // Although and expensive operation, I appened buffer char by char to have new line or tab space character included.
-            while (inf.get(c))
-                buffer += c;
+        hpxflow(string filename) {}
 
-            toVector();
-        }
+        void toVector(){}
 
-    void toVector(){
-        istringstream iss(buffer);
-        string word;
-        while(iss >> word) {
-            veco.push_back(word);
-        }
-        // for(int i = 0; i < veco.size(); i++){
-        //     cout << "Dump: " << veco[i] << endl;
-        // }
-        // return *this;
-    }
+        hpxflow &character_operation(char character, char replace_with){}
 
-    hpxflow &character_operation(char character, char replace_with){
-        using hpx::util::unwrapped;
-        using hpx::lcos::local::dataflow;
-        using hpx::parallel::for_each;
-        using hpx::parallel::par;
-        string app;
-        std::vector<char> data(buffer.begin(), buffer.end());
-        for_each(par, data.begin(), data.end(),
-            [&app, &character, &replace_with](char b)
-            {
-                if(to_string(b).c_str() == to_string(character).c_str()){
+        hpxflow &write_to_file(string filename){}
 
-                    app = app + replace_with;
+        hpxflow &dump() {}
 
-                } else{
+        template <typename F, typename... Args>
+        hpxflow &filter(int index, F fn, Args... args){}
 
-                    app = app + b;
+        template <typename L>
+        hpxflow &reduce(L fn, int vl){}
 
-                }
-        });
-        // }
-        buffer = app;
-        return *this;
-    }
-
-    hpxflow &write_to_file(string filename){
-        std::ofstream out(filename);
-        out << buffer;
-        out.close();
-        return *this;
-    }
-
-    hpxflow &dump() {
-        for (map<string, string>::iterator it=inter_map.begin(); it!=inter_map.end(); ++it)
-            cout << "First :"<< it->first << " Second: " << it->second << '\n' << endl;
-        return *this;
-    }
-
-    template <typename F, typename... Args>
-    hpxflow &filter(int index, F fn, Args... args){
-        for(size_t i = 0; i < buffer_pair_intermediate.size(); i++) {
-            if(index == 1){
-                if(!fn(buffer_pair_intermediate[i].first, args...)){
-                    buffer_pair_intermediate.erase(buffer_pair_intermediate.begin() + i);
-                }
-            }else if(index == 2){
-                if(!fn(buffer_pair_intermediate[i].second, args...)){
-                    buffer_pair_intermediate.erase(buffer_pair_intermediate.begin() + i);
-                }
-            }
-        }
-    }
-
-    template <typename L>
-    hpxflow &reduce(L fn, int vl){
-        fn(vl, buffer_pair_intermediate, inter_map);
-        return *this;  
-    }
-
-    template <typename T>
-    hpxflow &mapper(T t){
-        for (auto const &value : veco) {
-            buffer_pair.push_back(make_pair(t(value).first, t(value).second));
-            // auto v = t(value);
-            // cout << "First: " << v.first << " Second: " << v.second << endl;
-        }
-        buffer_pair_intermediate = buffer_pair;
-        return *this;
-        // return std::make_pair (10,20)
-    }
+        template <typename T>
+        hpxflow &mapper(T t){}
 
 };
 
