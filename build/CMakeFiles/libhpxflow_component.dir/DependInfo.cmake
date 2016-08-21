22494553f385416a7184a4c962c62bf3ccbe24ca
aalekh <aalekh.nigam@gmail.com>
2016-08-22 02:48:04 +0530

    Added Initial implementation of hypertable

---
+++ build/CMakeFiles/libhpxflow_component.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/algorithm/algorithm.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/algorithm/algorithm.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/algorithm/fileoperations.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/algorithm/fileoperations.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/algorithm/hashtable.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/algorithm/hashtable.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/core_layer/mapper.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/core_layer/mapper.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/core_layer/reduce.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/core_layer/reduce.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/dataflow/trigger.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/dataflow/trigger.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/dataflow/watermark.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/dataflow/watermark.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/dataflow/window.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/dataflow/window.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/HpxFlowPipelineSegment.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/HpxFlowPipelineSegment.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/HpxFlowPipelineSegmentBase.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/HpxFlowPipelineSegmentBase.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/HpxFlowPipelineSegmentBaseAdapter.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/HpxFlowPipelineSegmentBaseAdapter.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/HpxFlowPipelineTail.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/HpxFlowPipelineTail.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/PipeSegment.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/PipeSegment.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/blocking_queue.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/blocking_queue.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/count_down_latch.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/count_down_latch.cpp.o"
+  "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/src/pipeline/pipeline.cpp" "/Users/aalekhnigam/Documents/work/gsoc/hpxflow/build/CMakeFiles/libhpxflow_component.dir/src/pipeline/pipeline.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "HPX_COMPONENT_EXPORTS"
+  "HPX_COMPONENT_NAME=hpx_libhpxflow"
+  "HPX_COMPONENT_STRING=\"hpx_libhpxflow\""
+  "HYPERTABLE_CONFIG_FILE=\\\"/opt/hypertable/current/conf/hypertable.cfg\\\""
+  "HYPERTABLE_INSTALL_DIR=\\\"/opt/hypertable/current/\\\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/aalekhnigam/Downloads/hpx-0.9.11"
+  "/Users/aalekhnigam/Documents/work/hpx"
+  "/Users/aalekhnigam/Downloads/hpx-0.9.11/external/cache"
+  "/Users/aalekhnigam/Downloads/hpx-0.9.11/examples"
+  "/Users/aalekhnigam/Downloads/hpx-0.9.11/tests"
+  "/usr/local/include"
+  "/usr/local/Cellar/hwloc/1.11.2/include"
+  ".."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
