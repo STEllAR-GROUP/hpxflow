96631ae7f81fecaba687a7562f58e6d4fbe1ec6e
Aalekh Nigam <aalekh.nigam@gmail.com>
2016-08-29 12:00:44 +0530

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,24 @@
-#include "../src/hpxflow.h"
+#include <Common/Compat.h>
+#include <Common/Logger.h>
+#include <Common/System.h>
+
+#include <Hypertable/Lib/Key.h>
+#include <Hypertable/Lib/KeySpec.h>
+
+#include <ThriftBroker/Client.h>
+#include <ThriftBroker/gen-cpp/Client_types.h>
+#include <ThriftBroker/gen-cpp/HqlService.h>
+#include <ThriftBroker/ThriftHelper.h>
+#include <ThriftBroker/SerializedCellsReader.h>
+#include <ThriftBroker/SerializedCellsWriter.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+using namespace Hypertable;
+using namespace Hypertable::ThriftGen;
+using namespace std;
 
 #include <fstream>
 #include <iostream>
@@ -9,7 +29,10 @@
 #include <utility>
 #include <map>
 
-void counte(int index, vector<pair<string, string>> buf, map<string, string> inte){
+//#include "../src/hpxflow.h"
+#include "../src/dataflow/hypertableflow.h"
+
+void counte(int index, std::vector<std::pair<std::string, std::string> > buf, std::map<std::string, std::string> inte){
     int count;
     for(int j = 0; j < buf.size(); j++){
         if(index == 1) {
@@ -17,18 +40,18 @@ void counte(int index, vector<pair<string, string>> buf, map<string, string> int
                 count = stoi(buf[j].first);
                 count = count + 1;
                 inte.erase(buf[j].first);
-                inte[buf[j].first] = to_string(count);
+                inte[buf[j].first] = std::to_string(count);
             } else {
-                inte[buf[j].first] = to_string(1);
+                inte[buf[j].first] = std::to_string(1);
             }
         }else if(index == 2) {
             if(inte.find(buf[j].second) != inte.end() ){
                 count = stoi(buf[j].second);
                 count = count + 1;
                 inte.erase(buf[j].second);
-                inte[buf[j].second] = to_string(count);
+                inte[buf[j].second] = std::to_string(count);
             } else {
-                inte[buf[j].second] = to_string(1);
+                inte[buf[j].second] = std::to_string(1);
             }
         }
     }
@@ -36,12 +59,21 @@ void counte(int index, vector<pair<string, string>> buf, map<string, string> int
 }
 
 int main(){
-   hpxflow("text.txt").character_operation(' ', '\n').write_to_file("output.txt");
+
+    Thrift::Client *client = nullptr;
+
+    try {
+      client = new Thrift::Client("localhost", 15867);
+    }
+    catch (ClientException &e) {
+      cout << e.message << endl;
+      exit(1);
+    }
+   // hpxflow("text.txt").character_operation(' ', '\n').write_to_file("output.txt");
    // hpxflow("text.txt").operate([](string x){ return std::make_pair (x, "1");}).dump();
 
    // std::function<(int)> x1 = [=](int index) -> int{return a + b;};
-
-   cout << "Now the Filter Version \n\n\n" << endl;
-   hpxflow("text.txt").mapper([](string x){ return std::make_pair (x, "1");}).filter(1, [](string u){ return u.size() > 3;}).reduce(counte, 1).dump();
+   std::cout << "Now the Filter Version" << std::endl;
+   // hpxflow("text.txt").mapper([](std::string x){ return std::make_pair (x, "1");}).filter(1, [](std::string u){ return u.size() > 3;}).reduce(counte, 1).dump();
    return 0;
 }
